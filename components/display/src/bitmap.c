c5ca337c2f46f03791950156c5dbcbefa3051f28
ElBry <elvar.bryhni@gmail.com>
2025-06-16 14:19:22 +0200

    feat(display): functioning version of display with ssd1306, tested display did not have access to last 18 bits because of incompatible controller

--- components/display/src/bitmap.c
+++ components/display/src/bitmap.c
@@ -1,33 +1,32 @@
 #include "bitmap.h"
-#include "font8x8.h"
+#include "font5x7.h"
 #include <string.h>
 
-void build_text_bitmap_clipped(uint16_t slot_w,uint16_t slot_h, uint8_t *slot_bitmap, const char *str)
+
+void build_text_bitmap_clipped(uint16_t slot_w, uint16_t slot_h, uint8_t *slot_bitmap, const char *str, uint16_t str_size)
 {
-    memset(slot_bitmap, 0, slot_w * slot_h);
-    uint16_t cols = slot_w / 8;
-    uint16_t rows = slot_h / 8;
-    for (uint16_t row = 0; row < rows; row++) {
-        for (uint16_t ci = 0; ci < cols; ci++) {
-            char c = str[ci];
-            if (!c) break;
-            if ((unsigned char)c >= 128) c = '?';
-            uint8_t bits = font8x8_basic[(unsigned char)c][row];
-            uint16_t xoff = ci * 8;
-            uint16_t yoff = row * 8;
-            uint8_t *dst = slot_bitmap + yoff * slot_w + xoff;
-            for (uint16_t bit = 0; bit < 8; bit++) dst[bit] = (bits & (1 << bit)) ? 1 : 0;
+    if (slot_h != 8) return; // not supported yet
+    memset(slot_bitmap, 0, slot_w);
+    uint16_t max_chars = slot_w / 6; // one extra for blank space between lines
+    for (uint16_t ci = 0; ci < max_chars && ci < str_size; ci++) {
+        unsigned char c = (unsigned char)str[ci];
+        for (int col = 0; col < 5; col++) {
+            slot_bitmap[ci*6 + col] = font5x7[c+0][col];
         }
+        slot_bitmap[ci*6 + 5] = 0x00;    
     }
 }
-
-void scroll_bitmap_x(int16_t slot_w, int16_t slot_h, uint8_t *slot_bitmap, int16_t x_shift)
+/*
+uint8_t* get_shifted_bitmap(int16_t slot_w, int16_t slot_h, uint8_t *page_bitmap, int16_t x_shift)
 {
-    uint8_t row_buf[slot_w];
-    if (x_shift >  slot_w) x_shift =  slot_w;
-    if (x_shift < -slot_w) x_shift = -slot_w;
+
+    uint8_t shifted_buf[slot_w];
+    memset(shifted_buf, 0x00, slot_w);
+    for (int i = 0; i < slot_w; i++) {
+        
+    }
     for (int y = 0; y < slot_h; y++) {
-        uint8_t *row = slot_bitmap + y * slot_w;
+        uint8_t *row = page_bitmap + y * slot_w;
         memcpy(row_buf, row, slot_w);
         memset(row, 0, slot_w);
         for (int x = 0; x < slot_w; x++) {
@@ -36,9 +35,9 @@ void scroll_bitmap_x(int16_t slot_w, int16_t slot_h, uint8_t *slot_bitmap, int16
         }
     }
 }
+*/
 
-void invert_bitmap(int16_t slot_w, int16_t slot_h, uint8_t *slot_bitmap)
+void invert_bitmap(int16_t slot_w, uint8_t *slot_bitmap)
 {
-    int total = slot_w * slot_h;
-    for (int i = 0; i < total; i++) slot_bitmap[i] = slot_bitmap[i] ? 0 : 1;
+    for (int i = 0; i < slot_w; i++) slot_bitmap[i] = ~slot_bitmap[i];
 }
--- components/display/src/display.c
+++ components/display/src/display.c
@@ -4,10 +4,11 @@
 #include "esp_err.h"
 #include "esp_lcd_panel_vendor.h"
 #include "esp_lcd_panel_io.h"
+#include "esp_lcd_panel_ops.h"
+#include <string.h>
 
 
-// i2c_bus port 0 or 1. sda_pin and scl_pin check datasheet
-esp_lcd_panel_handle_t ssd1306_get_handle(i2c_port_num_t i2c_bus_port, gpio_num_t sda_pin, gpio_num_t scl_pin)
+lcd_handles ssd1306_get_handle(i2c_port_num_t i2c_bus_port, gpio_num_t sda_pin, gpio_num_t scl_pin)
 {
     i2c_master_bus_handle_t i2c_bus = NULL;
     i2c_master_bus_config_t bus_config = {
@@ -18,9 +19,11 @@ esp_lcd_panel_handle_t ssd1306_get_handle(i2c_port_num_t i2c_bus_port, gpio_num_
         .scl_io_num = scl_pin,
         .flags.enable_internal_pullup = true,
     };
+    
 
     ESP_ERROR_CHECK(i2c_new_master_bus(&bus_config, &i2c_bus));
-    esp_lcd_panel_io_handle_t io_handle = NULL;
+
+    lcd_handles handles = {.io = NULL, .panel = NULL};
     esp_lcd_panel_io_i2c_config_t io_config = {
         .dev_addr = SSD1306_I2C_HW_ADDR,
         .scl_speed_hz = LCD_PIXEL_CLOCK_HZ,
@@ -29,14 +32,59 @@ esp_lcd_panel_handle_t ssd1306_get_handle(i2c_port_num_t i2c_bus_port, gpio_num_
         .lcd_cmd_bits = LCD_CMD_BITS,
         .lcd_param_bits = LCD_CMD_BITS,
     };
-    ESP_ERROR_CHECK(esp_lcd_new_panel_io_i2c(i2c_bus, &io_config, &io_handle));
+    ESP_ERROR_CHECK(esp_lcd_new_panel_io_i2c(i2c_bus, &io_config, &handles.io));
 
-    esp_lcd_panel_handle_t panel_handle = NULL;
+    esp_lcd_panel_ssd1306_config_t ssd1306_spec = {
+        .height = 64,
+    };
     esp_lcd_panel_dev_config_t panel_config = {
         .bits_per_pixel = 1,
         .reset_gpio_num = SSD1306_PIN_NUM_RST,
+        .vendor_config = &ssd1306_spec,
     };
-    ESP_ERROR_CHECK(esp_lcd_new_panel_ssd1306(io_handle, &panel_config, &panel_handle));
-    return panel_handle;
+    ESP_ERROR_CHECK(esp_lcd_new_panel_ssd1306(handles.io, &panel_config, &handles.panel));
+    return handles;
 }
 
+esp_err_t lcd_start(esp_lcd_panel_handle_t panel)
+{   
+    esp_err_t err;
+    err = esp_lcd_panel_reset(panel);
+    if (err != ESP_OK) return err;
+    err = esp_lcd_panel_init(panel);
+    if (err != ESP_OK) return err;
+    err = esp_lcd_panel_disp_on_off(panel, true);
+    return err;
+}
+
+esp_err_t lcd_end(esp_lcd_panel_handle_t panel) 
+{
+    return esp_lcd_panel_del(panel);
+}
+
+
+esp_err_t clear_screen(esp_lcd_panel_handle_t panel) {
+    esp_err_t err;
+    static uint8_t blank_page[128];
+    memset(blank_page, 0x00, sizeof(blank_page));
+    for (int page = 0; page < 8; page++) {
+        int y0 = page * 8;
+        err = esp_lcd_panel_draw_bitmap(panel, 0, y0, 127, y0 +7, blank_page);
+        if(err != ESP_OK) return err;
+    }
+    return ESP_OK;
+}
+
+
+
+esp_err_t white_screen(esp_lcd_panel_handle_t panel) {
+    esp_err_t err;
+    static uint8_t white_page[128];
+    memset(white_page, 0xFF, sizeof(white_page));
+    for (int page = 0; page < 8; page++) {
+        int y0 = page * 8;
+        err = esp_lcd_panel_draw_bitmap(panel, 0, y0, 127, y0 +7, white_page);
+        if(err != ESP_OK) return err;
+    }
+    return ESP_OK;
+}
