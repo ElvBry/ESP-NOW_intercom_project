9e54f39dd5cc4cd5e1136c0ecdd2e89f0962a55e
ElBry <elvar.bryhni@gmail.com>
2025-06-23 15:42:22 +0200

    feat(menu_system): Working menusystem with example using ssd1306 display

--- components/menu_system/include/menu_system/menu_system.h
+++ components/menu_system/include/menu_system/menu_system.h
@@ -1,10 +1,32 @@
 #pragma once
 #include "menu_system/item_tree.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/queue.h"
+#include "esp_lcd_panel_io.h"
 #include <esp_err.h>
 
-static const char 
+#define MENU_EVT_QUEUE_SIZE 8
+#define MENU_MAX_ITEMS_PER_PAGE 7
+#define INVERTED 0 // Invert the display colors
 
-void create_folder();
+typedef enum {
+    MENU_EVT_NONE = 0,    // No event
+    MENU_EVT_BACK,        // Go back to parent menu
+    MENU_EVT_HOME,        // Go to the home menu
+    MENU_EVT_SELECT,      // Item selected
+    MENU_EVT_SCROLL_UP,   // Scroll up in the menu
+    MENU_EVT_SCROLL_DOWN, // Scroll down in the menu
+} menu_event_type_t;
 
-esp_err_t menu_system_init(, mainMenu, const char *str "mainMenu");
+
+typedef struct {
+    item_t *initial_menu;         // Pointer to the initial menu item
+    esp_lcd_panel_io_handle_t io; // LCD panel I/O handle
+} menu_handler_task_args_t;
+
+
+
+void menu_system_init(esp_lcd_panel_io_handle_t io);
+
+QueueHandle_t menu_system_get_event_queue(void);
 
---
+++ examples/menu_system/displayMenuSystem_example/main/displayMenuSystem.c
@@ -0,0 +1,40 @@
+#include "display/display.h"
+#include "menu_system/menu_system.h"
+#include "uart_handler.h"
+#include <esp_log.h>
+#include <esp_lcd_panel_io.h>
+#include <string.h>
+
+static const char* TAG = "displayMenuSystem";
+
+void app_main(void) {
+    lcd_handles_t handles = ssd1306_get_handles(0,21,22);
+    ESP_ERROR_CHECK(ssd1306_start(handles));
+    ESP_ERROR_CHECK(ssd1306_white_screen(handles.io));
+    ESP_ERROR_CHECK(ssd1306_clear_screen(handles.io));
+    menu_system_init(handles.io);
+    QueueHandle_t menu_event_queue = menu_system_get_event_queue();
+    assert(uart_handler_init());
+    QueueHandle_t q = uart_handler_get_queue();
+    command_t cmd;
+    menu_event_type_t evt;
+    while (xQueueReceive(q, &cmd, portMAX_DELAY)) {
+        if (strcmp(cmd.str, "none") == 0) {
+            evt = MENU_EVT_NONE;
+        } else if (strcmp(cmd.str, "back") == 0) {
+            evt = MENU_EVT_BACK;
+        } else if (strcmp(cmd.str, "home") == 0) {
+            evt = MENU_EVT_HOME;
+        } else if (strcmp(cmd.str, "sel") == 0) {
+            evt = MENU_EVT_SELECT;
+        } else if (strcmp(cmd.str, "up") == 0) {
+            evt = MENU_EVT_SCROLL_UP;
+        } else if (strcmp(cmd.str, "down") == 0) {
+            evt = MENU_EVT_SCROLL_DOWN;
+        } else {
+            ESP_LOGW(TAG, "Unknown command: %s", cmd.str);
+            continue;
+        }
+        xQueueSend(menu_event_queue, &evt, portMAX_DELAY);
+    }
+}
